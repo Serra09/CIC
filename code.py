7820b7b703b3d1bff3fa0c06bcad443c4de753bd
Serra <javiergarciapb@gmail.com>
2023-02-15 19:43:24 +0100

    code update

--- code.py
+++ code.py
@@ -1,11 +1,37 @@
 import json
 import os
 
-f = open(os.getcwd() + '/Documents/CIC/prueba.json')
+#Variable declaration
+list = []
+aux = ''
+n = 0
+f = 0
 
-data = json.load(f)
+fp = open(os.getcwd() + '/Documents/CIC/prueba.txt')
 
-print(data)
+for i, line in enumerate(fp):
+    f = f+1
 
+f = int(f/13) #Number of 13 line paragraphs
+fp = open(os.getcwd() + '/Documents/CIC/prueba.txt') #Resets the file pointer
 
+for count in range(f):
+    line_numbers = [n, n+1, n+2, n+3, n+4, n+5, n+6, n+7, n+8, n+9, n+10, n+11, n+12]
+    for i, line in enumerate(fp):
 
+        if i in line_numbers:
+            aux = aux + line
+
+    fp2 = open(os.getcwd() + '/Documents/CIC/aux.json', 'w+')
+
+    fp2.write(aux)
+
+    json_aux = json.loads(aux)
+
+    list.append(json_aux)
+
+    n = n+13
+
+print(list)
+print(list[1]['src'][1]['x']) #:)
+#print(list[0]['src']) #:)
