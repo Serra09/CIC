0d440df650173bcc7f9e42316f2a73ee75e982b5
Serra <javiergarciapb@gmail.com>
2023-02-27 13:52:48 +0100

    graph

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
 import json
 import os
+import matplotlib.pyplot as plt
 
+#FIRST PART: OBTAINING DATA OUT OF THE GENERATED loc.txt
 #Variable declaration
 list = []
 aux = ''
@@ -19,7 +21,7 @@ fp.seek(0,0) #Resets the file pointer
                        
 for count in range(f):
     line_numbers = [n, n+1, n+2, n+3, n+4, n+5, n+6, n+7, n+8, n+9, n+10, n+11, n+12]
-    i = 0
+    #i = 0
     aux = ''
     fp.seek(0,0)
 
@@ -34,8 +36,40 @@ for count in range(f):
     
     n = n+13
 
-    
-#print(list)
-print(list[15]['src'][1]['y'])
+print(list[15]['src'][2]['y'])
 #print(list[2]['timeStamp'])
+
+#SECOND PART: REPRESENTING THIS DATA IN 4 QUADRANTS
+#Variable declaration
+timestamp = []
+E = []
+Emax = 0
+nPots = 8
+
+#Timestamp array gets created
+for i in range(len(list)):
+
+    timestamp.append(list[i]['timeStamp'])
+
+#E array gets created
+for i in range(len(list)):
+
+    for j in range(nPots):
+
+        if list[i]['src'][j]['x'] > 0 and list[i]['src'][j]['y'] > 0:
+
+            if list[i]['src'][j]['E'] > Emax:
+
+                Emax = list[i]['src'][j]['E']
+
+    E.append(Emax)
+    Emax = 0
+
+#Plotting graph
+plt.plot(timestamp, E)
+plt.title("Quadrant 1 (Driver)")
+plt.xlabel("Timestamps")
+plt.ylabel("E")
+plt.show()
+
 
