1324ef60ac18858f895b9c81f3906966863e41ba
Serra <javiergarciapb@gmail.com>
2023-03-15 14:36:50 +0100

    ddd

--- code.py
+++ code.py
@@ -3,6 +3,7 @@ import os
 import matplotlib.pyplot as plt
 
 #FIRST PART: OBTAINING DATA OUT OF THE GENERATED loc.txt
+
 #Variable declaration
 list = []
 aux = ''
@@ -21,7 +22,6 @@ fp.seek(0,0) #Resets the file pointer
                        
 for count in range(f):
     line_numbers = [n, n+1, n+2, n+3, n+4, n+5, n+6, n+7, n+8, n+9, n+10, n+11, n+12]
-    #i = 0
     aux = ''
     fp.seek(0,0)
 
@@ -40,6 +40,7 @@ print(list[15]['src'][2]['y'])
 #print(list[2]['timeStamp'])
 
 #SECOND PART: REPRESENTING THIS DATA IN 4 QUADRANTS
+
 #Variable declaration
 timestamp = []
 E = []
@@ -51,7 +52,7 @@ for i in range(len(list)):
 
     timestamp.append(list[i]['timeStamp'])
 
-#E array gets created
+#Quadrant 1
 for i in range(len(list)):
 
     for j in range(nPots):
@@ -72,4 +73,73 @@ plt.xlabel("Timestamps")
 plt.ylabel("E")
 plt.show()
 
+#Resetting E
+E = []
 
+#Quadrant 2
+for i in range(len(list)):
+
+    for j in range(nPots):
+
+        if list[i]['src'][j]['x'] < 0 and list[i]['src'][j]['y'] > 0:
+
+            if list[i]['src'][j]['E'] > Emax:
+
+                Emax = list[i]['src'][j]['E']
+
+    E.append(Emax)
+    Emax = 0
+
+#Plotting graph
+plt.plot(timestamp, E)
+plt.title("Quadrant 2")
+plt.xlabel("Timestamps")
+plt.ylabel("E")
+plt.show()
+
+#Resetting E
+E = []
+
+#Quadrant 3
+for i in range(len(list)):
+
+    for j in range(nPots):
+
+        if list[i]['src'][j]['x'] < 0 and list[i]['src'][j]['y'] < 0:
+
+            if list[i]['src'][j]['E'] > Emax:
+
+                Emax = list[i]['src'][j]['E']
+
+    E.append(Emax)
+    Emax = 0
+
+#Plotting graph
+plt.plot(timestamp, E)
+plt.title("Quadrant 3")
+plt.xlabel("Timestamps")
+plt.ylabel("E")
+plt.show()
+
+E = []
+
+#Quadrant 4
+for i in range(len(list)):
+
+    for j in range(nPots):
+
+        if list[i]['src'][j]['x'] > 0 and list[i]['src'][j]['y'] < 0:
+
+            if list[i]['src'][j]['E'] > Emax:
+
+                Emax = list[i]['src'][j]['E']
+
+    E.append(Emax)
+    Emax = 0
+
+#Plotting graph
+plt.plot(timestamp, E)
+plt.title("Quadrant 4")
+plt.xlabel("Timestamps")
+plt.ylabel("E")
+plt.show()
